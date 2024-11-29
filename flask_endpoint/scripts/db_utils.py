fbfc583fb4a0a9d6e12a33d638e02fdeb76c4fa5
Rahmadiyan Muhammad <riansshole124@gmail.com>
2024-11-30 03:01:44 +0700

    fix db schema and secrets

--- flask_endpoint/scripts/db_utils.py
+++ flask_endpoint/scripts/db_utils.py
@@ -2,13 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-engine = create_engine('postgresql://user:password@localhost:5432/postgres_db', pool_pre_ping=True, pool_recycle=300)
+engine = create_engine(os.getenv('DATAENG_POSTGRES_URI'), pool_pre_ping=True, pool_recycle=300)
 
 def get_session():
     from scripts.models import Base
     Base.metadata.create_all(bind=engine)
-    # engine = create_engine(os.getenv('POSTGRES_URL_SOURCE'), pool_pre_ping=True, pool_recycle=300)
-    # engine = create_engine('postgresql://default:AVb2Io9pqyWm@ep-super-shape-a12kklqk.ap-southeast-1.aws.neon.tech:5432/verceldb', pool_pre_ping=True, pool_recycle=300)
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
     return SessionLocal()
 
