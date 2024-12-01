7c9559a16ff9c2f16c1b812b69f26f147bb2d298
Rahmadiyan Muhammad <riansshole124@gmail.com>
2024-12-01 23:54:54 +0700

    fix airflow compose, image, and entry point

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 airflow db init
 echo "AUTH_ROLE_PUBLIC = 'Admin'" >> webserver_config.py
-airflow connections add 'postgres_source' \
+airflow connections add 'source_postgres' \
 --conn-type 'postgres' \
 --conn-login $POSTGRES_SOURCE_USER \
 --conn-password $POSTGRES_SOURCE_PASSWORD \
 --conn-host $POSTGRES_HOST_SOURCE \
 --conn-port $POSTGRES_PORT_SOURCE \
 --conn-schema $POSTGRES_SOURCE_DB
-airflow connections add 'postgres_target' \
+airflow connections add 'main_postgres' \
 --conn-type 'postgres' \
 --conn-login $POSTGRES_USER \
 --conn-password $POSTGRES_PASSWORD \
 --conn-host $POSTGRES_CONTAINER_NAME \
 --conn-port $POSTGRES_PORT \
 --conn-schema $POSTGRES_DB
-airflow variables set 'source_params' \
-'{"host": "'$POSTGRES_HOST_SOURCE'", "port": '$POSTGRES_PORT_SOURCE', "user": "'$POSTGRES_SOURCE_USER'", "password": "'$POSTGRES_SOURCE_PASSWORD'", "database": "'$POSTGRES_SOURCE_DB'"}'
-airflow variables set 'target_params' \
-'{"host": "'$POSTGRES_CONTAINER_NAME'", "port": '$POSTGRES_PORT', "user": "'$POSTGRES_USER'", "password": "'$POSTGRES_PASSWORD'", "database": "'$POSTGRES_DB'"}'
+airflow connections add 'analysis_postgres' \
+--conn-type 'postgres' \
+--conn-login $POSTGRES_USER \
+--conn-password $POSTGRES_PASSWORD \
+--conn-host $POSTGRES_ANALYSIS_CONTAINER_NAME \
+--conn-port $POSTGRES_ANALYSIS_PORT \
+--conn-schema $POSTGRES_DB
+airflow variables set 'email_config' \
+--value '{"EMAIL_SENDER": "'$EMAIL_SENDER'", "EMAIL_PASSWORD": '$EMAIL_PASSWORD'}'
+
+airflow variables set 'email_recipient' \
+--value '{{"EMAIL_RECIPIENT": "'$EMAIL_RECIPIENT'"}}'
+
 export SPARK_FULL_HOST_NAME="spark://$SPARK_MASTER_HOST_NAME"
 airflow connections add 'spark_main' \
 --conn-type 'spark' \
