158706814b03e7f0fe612b8160ff8280cc22051e
Rahmadiyan Muhammad <riansshole124@gmail.com>
2024-12-02 16:59:29 +0700

    fix data migration bash script

--- bash_scripts/data_dump.sh
+++ bash_scripts/data_dump.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Array of tables to dump
-tables=("fact_history" "dim_album" "dim_song" "dim_artist")
+# Check if any tables were provided as arguments
+if [ $# -eq 0 ]; then
+    echo "Error: No tables specified"
+    echo "Usage: $0 table1 table2 ..."
+    exit 1
+fi
 
-# Loop through each table for dumping
-for TABLE_NAME in "${tables[@]}"
+# Loop through each table name provided as argument
+for TABLE_NAME in "$@"
 do
     echo "Dumping table ${TABLE_NAME}"
-    PGPASSWORD=${POSTGRES_SOURCE_PASSWORD} pg_dump --data-only --column-inserts -h ${POSTGRES_HOST_SOURCE} -U ${POSTGRES_SOURCE_USER} -d ${POSTGRES_SOURCE_DB} -t ${TABLE_NAME} > ../data/${TABLE_NAME}.sql
+    PGPASSWORD=${POSTGRES_SOURCE_PASSWORD} pg_dump --data-only --rows-per-insert=100 -h ${POSTGRES_HOST_SOURCE} -U ${POSTGRES_SOURCE_USER} -d ${POSTGRES_SOURCE_DB} -t ${TABLE_NAME} > /opt/airflow/data/${TABLE_NAME}.sql
 done
 echo "Dump completed"
--- bash_scripts/data_load.sh
+++ bash_scripts/data_load.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
 
-# Array of tables to dump
-tables=("fact_history" "dim_album" "dim_song" "dim_artist")
+# Check if any tables were provided as arguments
+if [ $# -eq 0 ]; then
+    echo "Error: No tables specified"
+    echo "Usage: $0 table1 table2 ..."
+    exit 1
+fi
 
 # Loop through each table for loading
-for TABLE_NAME in "${tables[@]}"
+for TABLE_NAME in "$@"
 do
     echo "Loading table ${TABLE_NAME}"
-    PGPASSWORD=${POSTGRES_PASSWORD} psql -h ${POSTGRES_CONTAINER_NAME} -U ${POSTGRES_USER} -d ${POSTGRES_DB} -f ../data/${TABLE_NAME}.sql
+    PGPASSWORD=${POSTGRES_PASSWORD} psql -h ${POSTGRES_CONTAINER_NAME} -U ${POSTGRES_USER} -d ${POSTGRES_DB} -f /opt/airflow/data/${TABLE_NAME}.sql
+
+    # Check if the last command was successful
+    if [ $? -ne 0 ]; then
+        echo "Error occurred while loading table ${TABLE_NAME}"
+        exit 1
+    fi
 done
+
 echo "Load completed"
